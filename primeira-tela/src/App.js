f8c632aaa5a87bfec009d1a2ac174fc8e9535c22
Gabriel Fenelon <gabriel.fenelon.biel@gmail.com>
2025-10-31 16:39:42 -0300

    Alterações e criaçao de novos arquivos

--- primeira-tela/src/App.js
+++ primeira-tela/src/App.js
@@ -1,14 +1,20 @@
-import CadastroClientes from './components/CadastroClientes';
-//import './styles.css/Nome.css';
-//import Nome from './components/Nome';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CadastroClientes from "./components/CadastroClientes";
+import NovoCadastro from "./components/NovoCadastro";
+import React from "react";
 
-function App(){
-
-  return(
-    <div>
-      <CadastroClientes />
-    </div>
-  ); 
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Rota para a lista de clientes (path foi mudado de /CadastroClientes para /clientes) */}
+        <Route path="/" element={<CadastroClientes />} />
+        
+        {/* ✅ NOVO: A rota do novo cadastro deve ser "/clientes/novo" para corresponder à navegação */}
+        <Route path="/clientes/novo" element={<NovoCadastro />} /> 
+        
+        {/* A rota /NovoCadastro (antiga) não é mais necessária */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App;
